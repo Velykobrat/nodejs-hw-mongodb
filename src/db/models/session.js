a79df9560c19e0687eaf8df05a97d1b156e3d974
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-10-02 13:14:47 +0300

    hw5-auth init
    // src/db/models/user.js
    // src/db/models/session.js

---
+++ src/db/models/session.js
@@ -0,0 +1,32 @@
+// src/db/models/session.js
+
+const sessionSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+    refreshTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Session = mongoose.model('Session', sessionSchema);
+
+module.exports = Session;
---
+++ src/db/models/user.js
@@ -0,0 +1,28 @@
+// src/db/models/user.js
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address.'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } // автоматично додає createdAt і updatedAt
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
